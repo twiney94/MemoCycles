13dd4358e4443a5208a026114aa7ac0128e50a09
Yassine BEKIOUI <mp482a@hotmail.fr>
2024-02-20 23:30:42 +0100

    fixed wrong "it" usage

--- domain/model/SessionSchema.js
+++ domain/model/SessionSchema.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const sessionSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   categories: [String],
-  duration: Number, // Durée de la session en minutes
+  duration: Number,
   createdAt: { type: Date, default: Date.now }
 });
 
